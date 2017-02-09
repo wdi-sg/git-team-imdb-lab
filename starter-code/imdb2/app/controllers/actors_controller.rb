e665205d9ba9630081019f1c26d90a0ca1a6ccf3
benedictkohtc <benedictkohtc@gmail.com>
2017-02-09 14:04:16 +0800

    added actor and movie controllers

--- starter-code/imdb2/app/controllers/actors_controller.rb
+++ starter-code/imdb2/app/controllers/actors_controller.rb
@@ -1,3 +1,22 @@
 class ActorsController < ApplicationController
+    def show
+    end
 
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+
+    def actor_params
+        params.require(:actor).permit(:name, :year_of_birth, :thumbnail)
+    end
 end
--- starter-code/imdb2/app/controllers/movies_controller.rb
+++ starter-code/imdb2/app/controllers/movies_controller.rb
@@ -1,3 +1,25 @@
 class MoviesController < ApplicationController
+    def index
+    end
 
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie).permit(:title, :summary, :youtube_embeded_id, :thumbnail)
+    end
 end
