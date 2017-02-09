a8c86a60c44aee5b5007041ad720cebfb3ba2fd1
top-banana <larissa.typ@gmail.com>
2017-02-09 14:01:49 +0800

    initial commit

--- starter-code/imdb2/app/models/user.rb
+++ starter-code/imdb2/app/models/user.rb
@@ -1,2 +1,13 @@
+# **Student 2** should create a new branch called `review-user-models` and fill in the Review and an User models:
+#   - Reviews belong to movies; they also belong to users
+#   - Users have many reviews
+#   - The user model should have a `full_name` method that returns an interpolated string with the user's `first_name` and `last_name`
+
 class User < ActiveRecord::Base
+  has_many :reviews
+
+  def full_name(first_name, last_name)
+    first_name + last_name
+  end
+
 end
